6e999d83850020f75fab18c6e0ef3f04e3918269
DekyyEx <vlad.moor123@mail.ru>
2024-07-02 15:42:06 +0300

    don't working chat

--- auth-service/src/main/java/org/example/authservice/exceprion/UsernameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package org.example.authservice.exceprion;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
